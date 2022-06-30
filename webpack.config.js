a0a95f92f68d603e13f2bd5bb8e91f43b7dbd6ee
joodb <joodb0126@gmail.com>
2022-06-30 13:25:56 +0900

    커밋 정보 추가

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 //const myLoader = require('./myLoader');
 const webpack = require('webpack');
+const childProcess = require('child_process');
+
 
 module.exports = {
     mode: 'development',
@@ -38,11 +40,16 @@ module.exports = {
             }
         ]
     },
+    //toLocaleString : 날짜의 문자열 표현을 지역의 언어에 맞는 형식으로 반환합니다.
+    // banner: '마지막 빌드 시간은 ' + new Date().toLocaleString() + ' 입니다.'
 
     plugins: [
         new webpack.BannerPlugin({
-            //toLocaleString : 날짜의 문자열 표현을 지역의 언어에 맞는 형식으로 반환합니다.
-        banner: '마지막 빌드 시간은 ' + new Date().toLocaleString() + ' 입니다.'
+            banner: `
+                Commit version : ${childProcess.execSync('git rev-parse --short HEAD')}
+                Committer name : ${childProcess.execSync('git config user.name')}
+                Commit Date : ${new Date().toLocaleString()}
+            `
         })
-   ]
+    ]
 }
